7e205ffa0536d815f85f45111ae7a0f3165d03a2
piotrlipiarz <14161641+piotr-lipiarz@users.noreply.github.com>
2022-11-20 21:04:51 +0100

    working k8s cluster on localhost

--- client/src/App.js
+++ client/src/App.js
@@ -11,7 +11,7 @@ function App() {
         <header className="App-header">
           <img src={logo} className="App-logo" alt="logo" />
           <p>
-            Żabki Fib Calculator
+            Żabki Fib Calculator version 4
           </p>
           <Link to="/">Home</Link>
           <Link to="/otherpage">Other Page</Link>
