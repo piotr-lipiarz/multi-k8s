c461bca175a642ab1cdedb6a6e7de6f9e2bbe6fa
piotrlipiarz <14161641+piotr-lipiarz@users.noreply.github.com>
2022-11-22 16:49:39 +0100

    added deployment scripts

---
+++ deploy.sh
@@ -0,0 +1,22 @@
+# Prerequisites
+#	logged in to Docker CLI
+#	authorized with kubectl CLI 
+
+# Build images
+docker build -t piotrlipiarz/multi-client:latest -t piotrlipiarz/multi-server:$SHA -f ./client/Dockerfile ./client
+docker build -t piotrlipiarz/multi-server:latest -t piotrlipiarz/multi-server:$SHA -f ./server/Dockerfile ./server
+docker build -t piotrlipiarz/multi-worker:latest -t piotrlipiarz/multi-worker:$SHA -f ./worker/Dockerfile ./worker
+
+# Push to Docker Hub
+docker push piotrlipiarz/multi-client:latest
+docker push piotrlipiarz/multi-server:latest
+docker push piotrlipiarz/multi-worker:latest
+docker push piotrlipiarz/multi-client:$SHA
+docker push piotrlipiarz/multi-server:$SHA
+docker push piotrlipiarz/multi-worker:$SHA
+
+# Apply K8s to cluster
+kubectl apply -f k8s
+kubectl set image deployments/server-deployment server=piotrlipiarz/multi-server:$SHA
+kubectl set image deployments/client-deployment client=piotrlipiarz/multi-client:$SHA
+kubectl set image deployments/worker-deployment worker=piotrlipiarz/multi-worker:$SHA
